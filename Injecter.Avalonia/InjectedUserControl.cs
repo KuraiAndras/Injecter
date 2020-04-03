de4e37d5d7da3f504bdc023029d6472c4e404125
Kurai András <k.andras9642@gmail.com>
2020-04-04 00:21:51 +0200

    Add injected user control, composition root

---
+++ Injecter.Avalonia/InjectedUserControl.cs
@@ -0,0 +1,38 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Injecter.Avalonia
+{
+#pragma warning disable SA1402 // File may only contain a single type
+    public abstract class InjectedUserControl : UserControl
+    {
+        protected InjectedUserControl() =>
+            Scope = CompositionRoot.ServiceProvider.GetRequiredService<IInjecter>().InjectIntoType(GetType(), this);
+
+        protected IServiceScope Scope { get; }
+
+        protected override void OnDetachedFromVisualTree(VisualTreeAttachmentEventArgs e)
+        {
+            base.OnDetachedFromVisualTree(e);
+
+            Scope?.Dispose();
+        }
+    }
+
+    public abstract class InjectedUserControl<TViewModel> : InjectedUserControl
+    {
+        protected InjectedUserControl() => ViewModel = Scope.ServiceProvider.GetRequiredService<TViewModel>();
+
+        protected TViewModel ViewModel { get; }
+
+        protected override void OnDetachedFromVisualTree(VisualTreeAttachmentEventArgs e)
+        {
+            base.OnDetachedFromVisualTree(e);
+
+            if (ViewModel is IDisposable disposable) disposable.Dispose();
+        }
+    }
+#pragma warning restore SA1402 // File may only contain a single type
+}
---
+++ Injecter/CompositionRoot.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Injecter
+{
+    public static class CompositionRoot
+    {
+        public static IServiceProvider ServiceProvider { get; set; } = new ServiceCollection().AddInjecter().BuildServiceProvider();
+    }
+}
