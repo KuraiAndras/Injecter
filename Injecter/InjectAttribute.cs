2ed5d263d4869cd55e905811fb968351e03cd51e
Kurai András <k.andras9642@gmail.com>
2020-04-03 22:08:59 +0200

    Add implementation

---
+++ Injecter/InjectAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Injecter
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Method | AttributeTargets.Property)]
+    public sealed class InjectAttribute : Attribute
+    {
+    }
+}
