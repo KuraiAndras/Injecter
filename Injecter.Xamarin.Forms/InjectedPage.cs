a239ff4255a59801b27b1f8391fba6ee946c97ed
Kurai András <k.andras9642@gmail.com>
2020-04-04 12:20:41 +0200

    Add forms project

---
+++ Injecter.Xamarin.Forms/InjectedPage.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using Xamarin.Forms;
+
+namespace Injecter.Xamarin.Forms
+{
+    public abstract class InjectedPage : Page
+    {
+        protected InjectedPage() => Scope = CompositionRoot.ServiceProvider.GetRequiredService<IInjecter>().InjectIntoType(GetType(), this);
+
+        protected IServiceScope Scope { get; }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+
+            Scope?.Dispose();
+        }
+    }
+
+    public abstract class InjectedPage<TViewModel> : InjectedPage
+    {
+        [Inject] protected TViewModel ViewModel { get; } = default;
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+
+            if (ViewModel is IDisposable disposable) disposable.Dispose();
+        }
+    }
+}
