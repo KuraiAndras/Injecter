392855a1b272b77cd376f6ec59004e3f7db2a310
Kurai András <k.andras9642@gmail.com>
2020-04-06 19:12:14 +0200

    Injecter 2.0.0

--- Injecter/IInjecter.cs
+++ Injecter/IInjecter.cs
@@ -5,6 +5,12 @@ namespace Injecter
 {
     public interface IInjecter
     {
+        /// <summary>
+        /// Injects services into members marked with <see cref="InjectAttribute"/>.
+        /// </summary>
+        /// <param name="type">Type of the object to inject into.</param>
+        /// <param name="instance">The instance which receives injection.</param>
+        /// <returns><see cref="IServiceScope"/> that gets created during injection.</returns>
         IServiceScope InjectIntoType(Type type, object instance);
     }
 }
--- Injecter/InjecterExtensions.cs
+++ Injecter/InjecterExtensions.cs
@@ -7,17 +7,19 @@ namespace Injecter
     {
         public static IServiceCollection AddInjecter(this IServiceCollection services, Action<InjecterOptions> optionsBuilder = null)
         {
+            if (services is null) throw new ArgumentNullException(nameof(services));
+
             var options = new InjecterOptions();
 
             optionsBuilder?.Invoke(options);
 
-            services.AddSingleton<InjecterOptions>();
+            services.AddSingleton(options);
             services.AddSingleton<IInjecter, Injecter>();
 
             return services;
         }
 
-        public static IServiceScope InjectIntoType<T>(this IInjecter injecter, object instance)
+        public static IServiceScope InjectIntoType<T>(this IInjecter injecter, T instance)
         {
             if (injecter is null) throw new ArgumentNullException(nameof(injecter));
 
