62c8d9b0a91d32c984ff4d2547447247a2042e5a
Kurai András <k.andras9642@gmail.com>
2020-04-12 16:08:44 +0200

    Add example project
    Add stuff to ui
    
    
    fix stuff

---
+++ Injecter.UwpExample/Services/IItemService.cs
@@ -0,0 +1,11 @@
+﻿using Injecter.UwpExample.Model;
+using System.Collections.Immutable;
+using System.Threading.Tasks;
+
+namespace Injecter.UwpExample.Services
+{
+    public interface IItemService
+    {
+        Task<IImmutableSet<Item>> GetAllItemsAsync();
+    }
+}
