ab6ade01ed33e16767dbe3b9e88f58c192342f89
Kurai András <k.andras9642@gmail.com>
2020-04-14 17:23:12 +0200

    TagetObjectGetsInjection

--- Injecter.Tests/InjecterTests.cs
+++ Injecter.Tests/InjecterTests.cs
@@ -1,12 +1,38 @@
-﻿using Xunit;
+﻿using Injecter.Tests.Arrangers;
+using Injecter.Tests.Arrangers.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using Xunit;
 
 namespace Injecter.Tests
 {
     public class InjecterTests
     {
         [Fact]
-        public void Test1()
+        public void TargetObjectGetsInjection()
         {
+            // Arrange
+            var (injecter, _) = CreateInjecter(services => services.AddSingleton<ISimpleService, SimpleService>());
+
+            // Act
+            var target = new SimpleInjectTarget();
+            _ = injecter.InjectIntoType(target);
+
+            // Assert
+            Assert.True(target.IsServiceNotNull);
+        }
+
+        private static (IInjecter injecter, ServiceProvider serviceProvider) CreateInjecter(Action<IServiceCollection> configureServices, Action<InjecterOptions>? optionsBuilder = default)
+        {
+            var services = new ServiceCollection();
+
+            configureServices(services);
+
+            services.AddInjecter(optionsBuilder);
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            return (serviceProvider.GetRequiredService<IInjecter>(), serviceProvider);
         }
     }
 }
