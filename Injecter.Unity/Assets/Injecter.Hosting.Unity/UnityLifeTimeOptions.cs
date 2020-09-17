b5d9346cea5ef980f440054fdbd6a2812b48a4f0
Kurai András <k.andras9642@gmail.com>
2020-09-17 17:39:52 +0200

    Add unity lifetime

---
+++ Injecter.Unity/Assets/Injecter.Hosting.Unity/UnityLifeTimeOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace Injecter.Hosting.Unity
+{
+    public sealed class UnityLifeTimeOptions
+    {
+        /// <summary>
+        /// Indicates if host lifetime status messages should be supressed such as on startup.
+        /// The default is false.
+        /// </summary>
+        public bool SuppressStatusMessages { get; set; }
+    }
+}
