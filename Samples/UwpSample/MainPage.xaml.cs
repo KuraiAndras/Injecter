72a8a65be5cafc34d65dfcf12fbb0b7220b38d3f
Kurai András <k.andras9642@gmail.com>
2020-12-23 22:40:34 +0100

    Ad uwp project

---
+++ Samples/UwpSample/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace UwpSample
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage() => InitializeComponent();
+
+        private void myButton_Click(object sender, RoutedEventArgs e) => MyButton.Content = "Clicked";
+    }
+}
