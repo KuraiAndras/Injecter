037ab118193aa639ef6c3dcbdb4c8cb5cc70cb82
Kurai András <k.andras9642@gmail.com>
2021-02-14 20:46:49 +0100

    test attached property

---
+++ Tests/Injecter.Wpf.Tests/TestBase.cs
@@ -0,0 +1,16 @@
+using Microsoft.Extensions.DependencyInjection;
+using SampleLogic;
+using System;
+
+namespace Injecter.Wpf.Tests
+{
+    public abstract class TestBase : IDisposable
+    {
+        protected TestBase() =>
+            CompositionRoot.ServiceProvider = new ServiceCollection()
+                .AddSharedLogic()
+                .BuildServiceProvider();
+
+        public void Dispose() => ((IDisposable)CompositionRoot.ServiceProvider!).Dispose();
+    }
+}
