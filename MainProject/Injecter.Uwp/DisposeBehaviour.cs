3111a13dc750bda4e214d85e0cf5e406456a366b
Kurai András <k.andras9642@gmail.com>
2021-02-15 22:09:52 +0100

    add scoped property

---
+++ MainProject/Injecter.Uwp/DisposeBehaviour.cs
@@ -0,0 +1,20 @@
+﻿namespace Injecter.Uwp
+{
+    public enum DisposeBehaviour
+    {
+        /// <summary>
+        /// Dispose when the window containing the control is closed.
+        /// </summary>
+        OnWindowClose,
+
+        /// <summary>
+        /// Dispose when the control is unloaded. It is useful for ListViewItems and similar.
+        /// </summary>
+        OnUnloaded,
+
+        /// <summary>
+        /// Don't dispose automatically.
+        /// </summary>
+        Manual,
+    }
+}
--- MainProject/Injecter.Uwp/XamlInjecter.cs
+++ MainProject/Injecter.Uwp/XamlInjecter.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 using Windows.ApplicationModel;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 
 namespace Injecter.Uwp
@@ -20,6 +22,83 @@ namespace Injecter.Uwp
             if (DesignMode.DesignMode2Enabled || DesignMode.DesignModeEnabled) return;
 
             CompositionRoot.ServiceProvider.GetRequiredService<IInjecter>().InjectIntoType(d.GetType(), d, false);
+        }
+
+        public static readonly DependencyProperty InjectScopedProperty = DependencyProperty.RegisterAttached(
+            "InjectScoped",
+            typeof(DisposeBehaviour?),
+            typeof(XamlInjecter),
+            new PropertyMetadata(null, DoInjectionScoped));
+
+        public static void SetInjectScoped(UIElement element, DisposeBehaviour value) => element.SetValue(InjectScopedProperty, value);
+        public static DisposeBehaviour GetInjectScoped(UIElement element) => (DisposeBehaviour)element.GetValue(InjectScopedProperty);
+
+        private static void DoInjectionScoped(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (DesignMode.DesignMode2Enabled || DesignMode.DesignModeEnabled) return;
+
+            CompositionRoot.ServiceProvider
+                .GetRequiredService<IInjecter>()
+                .InjectIntoType(d.GetType(), d);
+
+            var behavior = (DisposeBehaviour?)e.NewValue;
+
+            var owner = (FrameworkElement)d;
+
+            switch (behavior)
+            {
+                case DisposeBehaviour.OnWindowClose:
+                    {
+                        void OnLoaded(object _, RoutedEventArgs __)
+                        {
+                            owner.Loaded -= OnLoaded;
+                            owner = null!;
+
+                            var window = Window.Current;
+
+                            void OnWindowClosed(object ___, CoreWindowEventArgs ____)
+                            {
+                                window.Closed -= OnWindowClosed;
+                                window = null;
+
+                                CleanUp(ref owner);
+                            }
+
+                            window!.Closed += OnWindowClosed;
+                        }
+
+                        owner.Loaded += OnLoaded;
+
+                        break;
+                    }
+                case DisposeBehaviour.OnUnloaded:
+                    {
+                        void OnControlUnloaded(object _, RoutedEventArgs __)
+                        {
+                            owner.Unloaded -= OnControlUnloaded;
+                            owner = null!;
+
+                            CleanUp(ref owner);
+                        }
+
+                        owner.Unloaded += OnControlUnloaded;
+
+                        break;
+                    }
+                case DisposeBehaviour.Manual: break;
+                default: throw new ArgumentOutOfRangeException(behavior.ToString(), behavior, "Dispose behaviour not found");
+            }
+        }
+
+        private static void CleanUp(ref FrameworkElement owner)
+        {
+            CompositionRoot.ServiceProvider
+                .GetRequiredService<IScopeStore>()
+                .DisposeScope(owner);
+
+            // ReSharper disable once SuspiciousTypeConversion.Global
+            if (owner is IDisposable disposable) disposable.Dispose();
+            owner = null!;
         }
     }
 }
