cb4d1507de24ccac8ef3106a1b82d8ef1ea2c93e
Kurai András <k.andras9642@gmail.com>
2021-12-17 00:09:25 +0100

    simple IHost hookup

---
+++ UnityProject/Injecter.Unity/Assets/Injecter.Hosting.Unity/InjectionHelper.cs
@@ -0,0 +1,30 @@
+﻿using Injecter.Unity;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Injecter.Hosting.Unity
+{
+    public static class InjectionHelper
+    {
+        public static void RegisterInjectionsOnSceneLoad(IServiceProvider serviceProvider)
+        {
+            void SceneLoaded(Scene scene, LoadSceneMode mode)
+            {
+                var createScopes = serviceProvider.GetRequiredService<SceneInjectorOptions>().CreateScopes;
+
+                serviceProvider.GetRequiredService<ISceneInjector>().InitializeScene(createScopes, scene, mode);
+            }
+
+            void OnQuitting()
+            {
+                Application.quitting -= OnQuitting;
+                SceneManager.sceneLoaded -= SceneLoaded;
+            }
+
+            SceneManager.sceneLoaded += SceneLoaded;
+            Application.quitting += OnQuitting;
+        }
+    }
+}
