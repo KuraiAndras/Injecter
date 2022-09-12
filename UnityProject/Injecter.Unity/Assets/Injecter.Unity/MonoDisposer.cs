b44ab5e985c267472ea1b3da247d006e7f7457c1
Kurai András <k.andras9642@gmail.com>
2022-09-13 00:03:42 +0200

    implement monoinjector

---
+++ UnityProject/Injecter.Unity/Assets/Injecter.Unity/MonoDisposer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Injecter.Unity
+{
+    [DefaultExecutionOrder(int.MaxValue)]
+    public sealed class MonoDisposer : MonoBehaviour
+    {
+        private List<MonoBehaviour> _owners;
+        private IScopeStore _store;
+
+        public void Initialize(List<MonoBehaviour> owners, IScopeStore store)
+        {
+            _owners = owners;
+            _store = store;
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Roslynator", "RCS1213:Remove unused member declaration.", Justification = "Unity method")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0079:Remove unnecessary suppression", Justification = "Unity method")]
+        private void OnDestroy()
+        {
+            if (_owners == null) return;
+
+            for (var i = 0; i < _owners.Count; i++)
+            {
+                _store.DisposeScope(_owners[i]);
+            }
+
+            _owners.Clear();
+            _owners = null;
+            _store = null;
+        }
+    }
+}
--- UnityProject/Injecter.Unity/Assets/Injecter.Unity/MonoInjector.cs
+++ UnityProject/Injecter.Unity/Assets/Injecter.Unity/MonoInjector.cs
@@ -1,8 +1,31 @@
-﻿using UnityEngine;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace Injecter.Unity
 {
+    [DefaultExecutionOrder(int.MinValue)]
     public sealed class MonoInjector : MonoBehaviour
     {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Roslynator", "RCS1213:Remove unused member declaration.", Justification = "Unity method")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0079:Remove unnecessary suppression", Justification = "Unity method")]
+        private void Awake()
+        {
+            var injecter = CompositionRoot.ServiceProvider.GetRequiredService<IInjecter>();
+
+            var components = GetComponents<MonoBehaviour>();
+
+            var owners = new List<MonoBehaviour>(components.Length);
+            for (var i = 0; i < components.Length; i++)
+            {
+                var component = components[i];
+                if (component == null || component == this) continue;
+
+                injecter.InjectIntoType(component.GetType(), component, true);
+                owners.Add(component);
+            }
+
+            gameObject.AddComponent<MonoDisposer>().Initialize(owners, CompositionRoot.ServiceProvider.GetRequiredService<IScopeStore>());
+        }
     }
 }
