cde26ae787b4060f58833809caa394b40634b2ad
Kurai András <k.andras9642@gmail.com>
2022-09-13 19:20:22 +0200

    add benchmark project

---
+++ Benchmarks/GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Design", "CA1050:Declare types in namespaces", Scope = "type", Target = "~T:Md5VsSha256")]
+[assembly: SuppressMessage("Design", "CA1812:Program is an internal class that is apparently never instantiated.If so, remove the code from the assembly. If this class is intended to contain only static members, make it static (Shared in Visual Basic)")]
