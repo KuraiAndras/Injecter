28c54bfd249bb9c7372a47978548775a874e7d4c
Kurai András <k.andras9642@gmail.com>
2022-09-15 23:18:39 +0200

    restore unity hosting project

---
+++ UnityProject/Injecter.Unity/Assets/Injecter.Hosting.Unity/UnityLifeTimeOptions.cs
@@ -0,0 +1,12 @@
+﻿#nullable enable
+namespace Injecter.Hosting.Unity
+{
+    public sealed class UnityLifeTimeOptions
+    {
+        /// <summary>
+        /// Indicates if host lifetime status messages should be suppressed such as on startup.
+        /// The default is false.
+        /// </summary>
+        public bool SuppressStatusMessages { get; set; }
+    }
+}
