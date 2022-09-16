4fc77b7c64f9d8b80817a297c9b716a44fc3075f
Kurai András <k.andras9642@gmail.com>
2022-09-16 19:34:50 +0200

    add doc comments, add `AddOwner`

--- UnityProject/Injecter.Unity/Assets/Injecter.Unity/MonoDisposer.cs
+++ UnityProject/Injecter.Unity/Assets/Injecter.Unity/MonoDisposer.cs
@@ -10,14 +10,25 @@ namespace Injecter.Unity
     [DisallowMultipleComponent]
     public sealed class MonoDisposer : MonoBehaviour
     {
-        private List<MonoBehaviour> _owners;
+        private List<MonoBehaviour> _owners = new List<MonoBehaviour>();
         private IScopeStore _store;
 
+        /// <summary>
+        /// Initializes the disposer. Called during the injection by <see cref="MonoInjector"/>
+        /// </summary>
+        /// <param name="owners">The inejcted members</param>
+        /// <param name="store"><see cref="IScopeStore"/> Shoul be the instance retrieved from the service provider</param>
         public void Initialize(List<MonoBehaviour> owners, IScopeStore store)
         {
             _owners = owners;
             _store = store;
         }
+
+        /// <summary>
+        /// Add a mew owner to dispose. Should be used when dynamically adding injected components to <see cref="GameObject"/>s
+        /// </summary>
+        /// <param name="owner">The injected member</param>
+        public void AddOwner(MonoBehaviour owner) => _owners.Add(owner);
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Roslynator", "RCS1213:Remove unused member declaration.", Justification = "Unity method")]
         [System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0079:Remove unnecessary suppression", Justification = "Unity method")]
