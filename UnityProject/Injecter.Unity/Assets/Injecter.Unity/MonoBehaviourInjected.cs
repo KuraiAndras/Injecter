f48204f16f2ab979ade4397c27a8555f034054d0
Kurai András <k.andras9642@gmail.com>
2022-10-14 13:23:59 +0200

    add inheritable injecting base class

---
+++ UnityProject/Injecter.Unity/Assets/Injecter.Unity/MonoBehaviourInjected.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using UnityEngine;
+
+namespace Injecter.Unity
+{
+    /// <summary>
+    /// Inheriting from this class will inject everything marked with the <see cref="InjectAttribute"/>
+    /// </summary>
+    public abstract class MonoBehaviourInjected : MonoBehaviour
+    {
+        [Inject] private IScopeStore _store;
+
+        protected IServiceScope Scope { get; private set; }
+
+        /// <summary>
+        /// Call the base function before accessing injected members
+        /// </summary>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Roslynator", "RCS1213:Remove unused member declaration.", Justification = "Unity method")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0079:Remove unnecessary suppression", Justification = "Unity method")]
+        protected virtual void Awake() => Scope = CompositionRoot.ServiceProvider.GetRequiredService<IInjecter>().InjectIntoType(this, true);
+
+        /// <summary>
+        /// Call the base function after accessing injected members
+        /// </summary>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Roslynator", "RCS1213:Remove unused member declaration.", Justification = "Unity method")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0079:Remove unnecessary suppression", Justification = "Unity method")]
+        protected virtual void OnDestroy()
+        {
+            _store.DisposeScope(this);
+
+            Scope = null;
+            _store = null;
+        }
+    }
+}
